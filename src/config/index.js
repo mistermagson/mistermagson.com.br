1fa523fb98fe3381e64219338f65eab60ceaf2c5
Magson Magalhaes <magson@gmail.com>
2021-04-09 13:30:21 -0400

    Update src/config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? `http://localhost:3000` : `https://urldeproducao`
+export const server = dev ? `http://localhost:3000` : `https://mistermagson.com.br`
